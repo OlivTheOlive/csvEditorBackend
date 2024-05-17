e25ec8236e2ba4768ed687c48fe4c7d9d21974c0
OlivTheOlive <111806530+OlivTheOlive@users.noreply.github.com>
2024-05-16 23:50:15 -0400

    added documentation

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,6 +1,17 @@
 const { parseCSV } = require("../models/csvModel");
 
 const uploadCSV = (req, res) => {
+  /**
+   * Handles a file upload request.
+   *
+   * Logs the received file upload request and the path of the uploaded file.
+   * Calls the `parseCSV` function to parse the CSV file and passes a callback function to handle the parsed data.
+   * Sends the parsed data as a JSON response to the client and logs that the response has been sent.
+   *
+   * @param {Object} req - The request object containing information about the file upload.
+   * @param {Object} res - The response object used to send the JSON response to the client.
+   * @returns {void}
+   */
   console.log("Received file upload request");
   const csvFilePath = req.file.path;
   console.log("Uploaded file path:", csvFilePath);
