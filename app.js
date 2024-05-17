c3192c54fc6b33fc9fa2d0683bb1cbd7e3a501e2
OlivTheOlive <111806530+OlivTheOlive@users.noreply.github.com>
2024-05-16 23:55:07 -0400

    added .env file

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+// external imports
 const express = require("express");
 const cors = require("cors");
 const uploadRoute = require("./Routes/uploadRoute");
 
+// internal imports
+require("dotenv/config");
+const API = process.env.API_URL;
+const PORT = process.env.PORT;
+
 const app = express();
-const port = 3030;
 
 // Allow CORS
 app.use(cors());
 
 // Use the upload routes
-app.use("/", uploadRoute);
+app.use(API, uploadRoute);
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
