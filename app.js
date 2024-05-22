1620837057ba79af2dde7c73589072bef9f9c22e
OlivTheOlive <111806530+OlivTheOlive@users.noreply.github.com>
2024-05-21 21:36:40 -0400

    added CSVdataDTO with get setters

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const Papa = require("papaparse"); // Library for parsing CSV data
 const cors = require("cors"); // Middleware for enabling Cross-Origin Resource Sharing (CORS)
 
 const app = express(); // Create an instance of the Express application
-
 const port = 3030;
 
 // allow CORS --> allowing requests from any origin to access the server
@@ -14,13 +13,36 @@ app.use(cors());
 // set up multer for handling file uploads
 const upload = multer({ dest: "uploads/" });
 
+// DTO class
+class CsvDataDTO {
+  // constructor that initializes the DTO with the given data
+  constructor(data) {
+    // Check if the data is already an array
+    // if data is an array, use it directly
+    // if data is not an array (e.g., an object), convert it to an array of values
+    this.data = Array.isArray(data) ? data : Object.values(data);
+  }
+  // Getter for the data property
+  get data() {
+    return this._data;
+  }
+
+  // Setter for the data property with validation
+  set data(newData) {
+    if (Array.isArray(newData)) {
+      this._data = newData;
+    } else {
+      throw new Error("Data must be an array");
+    }
+  }
+}
+
 // handle file upload
 app.post("/upload", upload.single("csvFile"), (req, res) => {
   // read the uploaded file
   console.log("Received file upload request");
   const csvFilePath = req.file.path;
   console.log("Uploaded file path:", csvFilePath);
-  const records = [];
 
   // parse CSV file
   const fileStream = fs.createReadStream(csvFilePath);
@@ -32,8 +54,12 @@ app.post("/upload", upload.single("csvFile"), (req, res) => {
       // process parsed data
       const data = result.data;
       console.log("Parsing data");
+
+      // Create the DTO with the parsed data
+      const csvDataDTO = new CsvDataDTO(data);
+
       // send processed data back to client
-      res.json(data);
+      res.json(csvDataDTO);
       console.log("Response sent to client");
     },
   });
